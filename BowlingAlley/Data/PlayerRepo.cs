d39d4c9a2d1200ce4023820d9e2a2c8abcd965fb
Jessica Campbell <jesscampb91@gmail.com>
2025-03-03 15:19:18 +0100

    Added create and read functions for players/members

--- BowlingAlley/Data/PlayerRepo.cs
+++ BowlingAlley/Data/PlayerRepo.cs
@@ -3,10 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BowlingAlley.Core;
 
 namespace BowlingAlley.Data
 {
-    class PlayerRepo
+    public class PlayerRepo: IPlayerRepo
     {
+        private readonly string filePath = "Data/members.json";
+        private List<Player> players;
+
+        public PlayerRepo()
+        {
+            players = LoadPlayers();
+        }
+
+        private List<Player> LoadPlayers()
+        {
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "[]");
+                return new List<Player>();
+            }
+
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Player>>(json) ?? new List<Player>();
+        }
+
+        private void SavePlayers()
+        {
+            var json = JsonConvert.SerializeObject(players, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (!players.Any(p => p.Name == player.Name))
+            {
+                players.Add(player);
+                SavePlayers();
+            }
+            else
+            {
+                throw new InvalidOperationException("Player already exists.");
+            }
+        }
+
+        public Player? GetPlayer(string name)
+        {
+            return players.FirstOrDefault(p => p.Name == name);
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return new List<Player>(players);
+        }
     }
 }
