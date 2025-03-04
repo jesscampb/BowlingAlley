7e5c027455079c26998e71bd9692418b0757b051
Jessica Campbell <jesscampb91@gmail.com>
2025-03-04 12:35:48 +0100

    Added fields and constructor to the Game class

--- BowlingAlley/Core/Game.cs
+++ BowlingAlley/Core/Game.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BowlingAlley.Core
 {
-    class Game
+    public class Game
     {
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
+        private readonly IGameMode _gameMode;
+
+        public Game(Player playerOne, Player playerTwo, IGameMode gameMode)
+        {
+            _playerOne = playerOne;
+            _playerTwo = playerTwo;
+            _gameMode = gameMode;
+        }
     }
 }
