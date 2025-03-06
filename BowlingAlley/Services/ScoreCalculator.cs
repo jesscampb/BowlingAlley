0f01136e45e298b24a480a1f81de843f9bc378f6
Jessica Campbell <jesscampb91@gmail.com>
2025-03-06 09:38:35 +0100

    Added class ScoreCalculator.

---
+++ BowlingAlley/Services/ScoreCalculator.cs
@@ -0,0 +1,13 @@
+﻿using BowlingAlley.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingAlley.Services
+{
+    public static class ScoreCalculator
+    {
+    }
+}
