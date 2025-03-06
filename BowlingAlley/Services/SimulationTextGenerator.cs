e81bf910a1371332f5b6527a6afe435ce50f1fba
Jessica Campbell <jesscampb91@gmail.com>
2025-03-06 12:13:03 +0100

    Added some methods in SimulationTextGenerator for displaying messages for player turns, scoring and winner announcements.

--- BowlingAlley/Services/SimulationTextGenerator.cs
+++ BowlingAlley/Services/SimulationTextGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BowlingAlley.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace BowlingAlley.Services
 {
-    class SimulationTextGenerator
+    public static class SimulationTextGenerator
     {
+        public static string PlayerTurnMessage(Player player)
+        {
+            return $"{player.Name}, it's your turn to play!";
+        }
+
+        public static string PlayerTurnPinsMessage(Player player, int pins)
+        {
+            return pins switch
+            {
+                0 => $"Oops! {player.Name} throws a gutter ball... No pins downed!",
+                10 => $"STRIKE!! {player.Name} knocks down all the pins!",
+                _ => $"{player.Name} knocks down {pins} pins!"
+            };
+        }
+
+        public static string PlayerTurnScoreMessage(Player player, int turnScore)
+        {
+            return $"{player.Name} scored {turnScore} points this turn.";
+        }
+
+        public static string FinalScoreMessage(Player player, int finalScore)
+        {
+            return $"{player.Name} scored {finalScore} points in total.";
+        }
+
+        public static string WinnerMessage(Player winner, int finalScore)
+        {
+            return $"Congratulations {winner.Name}! You won the game with a final score of {finalScore}!";
+        }
+
+        public static string TieMessage(int finalScore)
+        {
+            return $"It's a tie! Both players scored {finalScore} points.";
+        }
     }
 }
