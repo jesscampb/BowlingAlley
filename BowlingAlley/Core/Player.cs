29a3a55457a073d383c5e396bc4b252f95ee92bc
Jessica Campbell <jesscampb91@gmail.com>
2025-03-06 09:58:18 +0100

    Updated Player class with neccessary props for scoring purposes and added a method to add score after a turn.

--- BowlingAlley/Core/Player.cs
+++ BowlingAlley/Core/Player.cs
@@ -9,10 +9,17 @@ namespace BowlingAlley.Core
     public class Player
     {
         public string Name { get; set; }
+        public List<int> TurnScores { get; } = new List<int>();
+        public int TotalScore => TurnScores.Sum();
 
         public Player(string name)
         {
             Name = name;
+        }
+
+        public void AddTurnScore(int score)
+        {
+            TurnScores.Add(score);
         }
     }
 }
